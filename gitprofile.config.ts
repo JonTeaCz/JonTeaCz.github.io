797b0f6cc965979afecd1af41c1745fc838fdef9
Jonathan Cerniaz <123432807+JonTeaCz@users.noreply.github.com>
2025-05-03 22:00:14 -0700

    Update gitprofile.config.ts
    commented out the articles.
    
    Need to update:
    - projects
    - repositories
    - upload youtube video
    - update photo

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -178,13 +178,15 @@ const CONFIG = {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
   ],
-  */
+  
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
     username: 'arifszn', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
+  */
+  
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
